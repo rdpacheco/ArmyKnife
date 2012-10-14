ff9eac849bf4e77fc535532ac6258c8c68804416
Roman D Pacheco <rdpacheco+github@gmail.com>
2012-10-14 12:08:07 -0500

    More work on Utils and section cycling methods
    - Added (currently unused) util "propExists"
    - Tweaked other util implementations, like slice
    - Added goto, prev, next methods
    - Added autoRotate method
    - Added "reverse" option
    - Misc tweaks

--- jquery.armyKnife.2.0.js
+++ jquery.armyKnife.2.0.js
@@ -4,11 +4,11 @@
 *	ArmyKnife Object and jQuery Plugin 2.0
 *
 */
-var ArmyKnife	=	(function ($, document) {
+var ArmyKnife	=	(function ($, window, document) {
 	"use strict";
 
 	/**
-	
+
 	*
 	*	Private properties and methods
 	*
@@ -29,13 +29,27 @@ var ArmyKnife	=	(function ($, document) {
 				}
 			},
 			extend	:	function () {	// Relies on jQuery
-				return $.extend.apply(this, Utils.slice.call(arguments));
+				return $.extend.apply($, Utils.slice(arguments));
 			},
 			find	:	function (parent, selector) {	// Relies on jQuery
 				var $results	=	$(selector, parent);
-				return this.slice.call($results);
+				return this.slice($results);
 			},
-			slice	:	Array.prototype.slice
+			propExists	:	function (context, str) {
+				var parts	=	str.split("."),
+					passed	=	true;
+				Utils.each(parts, function (i, v) {
+					if (typeof context[v] === "undefined") {
+						passed	=	false;
+						return false;
+					}
+					context	=	context[v];
+				});
+				return passed;
+			},
+			slice	:	function (arr) {
+				return Array.prototype.slice.call(arr);
+			}
 		},
 		Vars	=	{
 		},
@@ -43,16 +57,16 @@ var ArmyKnife	=	(function ($, document) {
 			button	:	Utils.createElem("button").setAttribute("href", "#")
 		},
 		Methods	=	{
-			initAutoRotate	:	function () {
-				console.log("Initializing Auto Rotate");
-			},
 			prepSections	:	function () {
 				console.log("Prepping Sections");
+
+				// Run setSection on all sections
 				Utils.each(self.sections, function (i, e) {
 					if (i === self.options.startingSection) {
-						AK.setSection.active.call(e);
+						self.active	=	i;
+						AK.setSection.active(e);
 					} else {
-						AK.setSection.hidden.call(e);
+						AK.setSection.hidden(e);
 					}
 				});
 			}
@@ -84,6 +98,7 @@ var ArmyKnife	=	(function ($, document) {
 		"resizeSpeed"			:	200,
 		"autoRotate"			:	false,
 		"autoRotateDelay"		:	5000,
+		"reverse"				:	false,
 		"generateNav"			:	false,
 		"navType"				:	"empty",				//	[empty|numeric|text|custom]
 		"navItemSource"			:	function (s) {			//	REQUIRED WHEN navType = 'text' OR 'custom'; RESPECTIVE SECTION IS PASSED FOR EASY REFERENCE
@@ -110,18 +125,17 @@ var ArmyKnife	=	(function ($, document) {
 		"useContinue"			:	false
 	};
 	AK.setSection	=	{
-		active	:	function (i) {
-			console.log(this);
-			this.setAttribute("class", AK.prefix + "-section-active");
+		active	:	function (section, i, dir) {
+			section.setAttribute("class", AK.prefix + "-section-active");
 		},
-		inactive	:	function (i) {
-			this.setAttribute("class", AK.prefix + "-section-inactive");
+		inactive	:	function (section, i) {
+			section.setAttribute("class", AK.prefix + "-section-inactive");
 		},
-		hidden	:	function (i, dir) {
-			if (dir || dir < 0) {
-				this.setAttribute("class", AK.prefix + "-section-hiddenRight");
+		hidden	:	function (section, i, dir) {
+			if (dir && dir > 0) {
+				section.setAttribute("class", AK.prefix + "-section-hiddenLeft");
 			} else {
-				this.setAttribute("class", AK.prefix + "-section-hiddenLeft");
+				section.setAttribute("class", AK.prefix + "-section-hiddenRight");
 			}
 		}
 	};
@@ -132,8 +146,36 @@ var ArmyKnife	=	(function ($, document) {
 	*
 	*/
 	AK.prototype	=	{
+		autoRotate	:	function () {
+			var obj	=	this,
+				dir;
+
+			// Using this.options.autoRotate as flag to continue. May change this later
+			if (typeof obj.options.autoRotate !== "undefined" && obj.options.autoRotate !== false) {
+				dir	=	obj.options.reverse !== true ? "next" : "prev";
+				window.clearTimeout(obj.delay);
+				obj.delay	=	window.setTimeout(function () {
+					obj[dir]();
+					obj.autoRotate();
+				}, obj.options.autoRotateDelay);
+			}
+		},
 		generateNav	:	function () {
 			console.log("Generating Nav");
+		},
+		"goto"	:	function (num_index) {
+			var active		=	this.active,
+				sections	=	this.sections,
+				dir			=	active < num_index ? 1 : -1;
+			AK.setSection.hidden(sections[active], dir);
+			AK.setSection.active(sections[num_index], dir);
+
+			// Update active section index
+			this.active	=	num_index;
+
+			// Reset auto rotate cycle
+			this.options.autoRotate
+				&& this.autoRotate();
 		},
 		init	:	function (uOptions, callback) {
 			self	=	this;
@@ -152,15 +194,25 @@ var ArmyKnife	=	(function ($, document) {
 
 			// Init auto rotate (if needed)
 			options.autoRotate
-				&& Methods.initAutoRotate();
+				&& this.autoRotate();
 
 			// Run callback (if needed)
 			typeof callback === "function"
 				&& callback.call(this);
+		},
+		next	:	function () {
+			var active		=	this.active,
+				nextIndex	=	active + 1 < this.sections.length ? active + 1 : 0;
+			this["goto"](nextIndex);
+		},
+		prev	:	function () {
+			var active		=	this.active,
+				prevIndex	=	active - 1 >= 0 ? active - 1 : this.sections.length - 1;
+			this["goto"](prevIndex);
 		}
 	};
 	return AK;
-}(jQuery, document));
+}(jQuery, window, document));
 
 /**
 *
@@ -173,8 +225,8 @@ jQuery.plugin	=	function (name, obj) {
 		return this.each(function () {
 
 			// If data does not exist, create it
-			if (!$.data(this, name)) {
-				$.data(this, name, new obj(this, options, callback));
+			if (!jQuery.data(this, name)) {
+				jQuery.data(this, name, new obj(this, options, callback));
 			}
 		});
 	};
